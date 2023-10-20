925c54f3ef6470444f2be2ac6c0e9a3237927f08
joe62 <yunfei_z@163.com>
2023-10-20 11:04:52 +0800

    update gitpush.bat

--- gitpush.bat
+++ gitpush.bat
@@ -1,16 +1,6 @@
 @echo off
-
-title GIT提交
-color 3
-echo 当前目录是: %cd%
-echo;
-
-echo 开始添加变更:git add .
+title Commit
 git add .
-
-set /p declation=输入提交的commit信息:
-git commit -m "%declation%"
-echo
-
-echo 执行完毕！
-pause
+set /p m=Message:
+git commit -m "%m%"
+git push
