7434453e8043ede4920f88a28ff852477a953925
joe62 <yunfei_z@163.com>
2023-10-20 10:38:31 +0800

    update vite base to docs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"Ultimate-Tailwind-CSS-Handbook-learning",
+  base:"docs",
   plugins: [react()],
 })
